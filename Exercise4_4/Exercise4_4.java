83bfc97991b65d82bb153d9d7716a85c981f58fb
melikegcncodes <240541081@firat.edu.tr>
2025-12-03 20:30:58 +0300

    Update Exercise4_4.java

--- Exercise4_4/Exercise4_4.java
+++ Exercise4_4/Exercise4_4.java
@@ -1,7 +1,7 @@
 public class Exercise4_4 {
 
     public static int getNumber() {
-        return 42;
+        return 42; //
     }
 
     public static void sayHello() {
@@ -13,9 +13,19 @@ public class Exercise4_4 {
         // 1) Donus degerini kullanmayin -> ne oluyor?
         getNumber();
 
+         /* getNumber() metodu 42 değerini return ile döndürür.
+         Ancak biz bu değeri ekrana yazdırmazsak veya bir değişkene atamazsak görünmez.
+         return sadece değeri gönderir, ekrana yazdırmaz.
+         Bu yüzden getNumber(); şeklinde çağırınca 42 çıktısı elde etmeyiz. */
+
+
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
 
-        // Cevaplarinizi yorum olarak ekleyin.
+        /* System.out.println(sayHello() + 7);
+         HATA verir.
+         Çünkü sayHello() void turundedir, yani herhangi bir deger döndürmez.
+         Bu nedenle matematiksel bir ifade icinde kullanilamaz. */
+
     }
 }
